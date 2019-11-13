1caed088209fc55586433ae85c9c0ff69acab3e2
Long_Teng Ling <s3a632004@student.ncut.edu.tw>
2019-11-13 11:36:15 +0800

    設定頁面跳轉

--- app/Http/Controllers/AdminPostsController.php
+++ app/Http/Controllers/AdminPostsController.php
@@ -31,5 +31,6 @@ class AdminPostsController extends Controller
     public function store()
     {
         Post::create($request->all());
+        return redirect()->route('admin.posts.index');
     }
 }
